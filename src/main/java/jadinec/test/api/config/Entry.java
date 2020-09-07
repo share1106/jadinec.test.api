0623e287a0b6471f2c7ce7c526baee2a06b89917
ganggang <ganggang@DESKTOP-BTEHN8R>
2020-09-07 17:58:59 +0800

    更新代码1

--- src/main/java/jadinec/test/api/config/Entry.java
+++ src/main/java/jadinec/test/api/config/Entry.java
@@ -2,14 +2,22 @@ package jadinec.test.api.config;
 
 import org.testng.TestNG;
 
+import jadinec.test.api.cases.AddUserToGroupTest;
+import jadinec.test.api.cases.GetAppUpdateTest;
+import jadinec.test.api.cases.GetEngineeringItemPlaneTest;
+import jadinec.test.api.cases.GetEngineeringPlaneTest;
+import jadinec.test.api.cases.GetSigTest;
 import jadinec.test.api.cases.GetTokenTest;
+import jadinec.test.api.cases.GetUserTest;
 import jadinec.test.api.cases.VerificationCodeLoginTest;
 
 public class Entry {
 
 	public static void main(String[] args) {
 		TestNG testNG = new TestNG();
-		Class[] classes = { GetTokenTest.class,VerificationCodeLoginTest.class };
+		Class[] classes = { GetTokenTest.class,VerificationCodeLoginTest.class,
+				AddUserToGroupTest.class,GetAppUpdateTest.class,GetEngineeringItemPlaneTest.class,
+				GetEngineeringPlaneTest.class,GetSigTest.class,GetUserTest.class};
 		testNG.setTestClasses(classes);
 		testNG.run();
 
